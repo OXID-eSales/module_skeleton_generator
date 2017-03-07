85b7fca8e142e76725df7f8788d6d5bf207923fb
Dmitrijus Druteika <dmitrijus.druteika@oxid-esales.com>
2017-03-07 15:33:01 +0200

    #PSGEN-229: Clean up of old testing lib files.

--- copy_this/modules/oxps/modulegenerator/tests/unit/testdata/testdata_remove.sql
+++
@@ -1,7 +0,0 @@
--- Test data removal script
-
--- Sample CMS snippet deletion
-DELETE FROM `oxcontents` WHERE `OXID` = 'oxpstestident';
-
--- Model related test table
-DROP TABLE IF EXISTS `testspecial_offer`;

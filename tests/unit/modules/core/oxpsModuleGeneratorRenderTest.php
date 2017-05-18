d764b472c9263c29faf86d21bf3678b453c41b43
Sergej Voronov <sergej@voronov.lt>
2017-05-18 12:54:52 +0300

    #PSGEN-229: Big fix of \oxpsModuleGeneratorRenderTest::testRenderModuleFiles test method

--- tests/unit/modules/core/oxpsModuleGeneratorRenderTest.php
+++ tests/unit/modules/core/oxpsModuleGeneratorRenderTest.php
@@ -76,54 +76,58 @@ class oxpsModuleGeneratorRenderTest extends OxidTestCase
 
     /**
      * Test expect the following sequence of...
-     *
-     * ----------------------------------------------------------------------------------------------------------
-     *  Rename file name                 => Template path                                FS exec     Smarty exec
-     * ----------------------------------------------------------------------------------------------------------
-     *  'oxpsmymodule_de_lang.php'       => 'translations/de/oxpsmodule_lang.php.tpl',   0-1         1-5
-     *  'oxpsmymodule_en_lang.php'       => 'translations/en/oxpsmodule_lang.php.tpl',   2-3         6-10
-     *  'oxpsmymodulemodule.php'         => 'core/oxpsModule.php.tpl',                   4-5         11-15
-     *  'docs/install.sql',                                                              6           16-20
-     *  'docs/README.txt',                                                               7           21-25
-     *  'docs/uninstall.sql',                                                            8           26-30
-     *  'oxpsmymodule_admin_de_lang.php' => 'views/admin/de/oxpsmodule_lang.php.tpl',    9-10        31-35
-     *  'oxpsmymodule_admin_en_lang.php' => 'views/admin/en/oxpsmodule_lang.php.tpl',    11-12       36-40
-     *  'metadata.php'                   => 'metadata.php.tpl',                          13-14       41-45
-     *  'models/oxpsmymoduleoxarticle.php',                                              15          46-50
-     *  'controllers/oxpsmymodulepage.php',                                              16          51-55
-     *  'models/oxpsmymoduleitem.php',                                                   17          56-60
-     * ----------------------------------------------------------------------------------------------------------
+     * @see https://oxid-esales.atlassian.net/browse/PSGEN-229 attached files for sequence revision
+     * -----------------------------------------------------------------------------------------------
+     *  Rename file name                    => Template path
+     * -----------------------------------------------------------------------------------------------
+     *  'oxpsmymodule_de_lang.php'          => 'Application/translations/de/oxpsModule_lang.php.tpl',
+     *  'oxpsmymodule_en_lang.php'          => 'Application/translations/en/oxpsModule_lang.php.tpl',
+     *  'oxpsmymodulemodule.php'            => 'Core/oxpsModule.php.tpl',
+     *  'docs/install.sql',
+     *  'docs/README.txt',
+     *  'docs/uninstall.sql',
+     *  'oxpsmymodule_admin_de_lang.php'    => 'Application/views/admin/de/oxpsModule_lang.php.tpl',
+     *  'oxpsmymodule_admin_en_lang.php'    => 'Application/views/admin/en/oxpsModule_lang.php.tpl',
+     *  '.ide-helper.php',                  => '.ide-helper.php.tpl',
+     *  'composer.json',                    => 'composer.json.tpl',
+     *  'metadata.php'                      => 'metadata.php.tpl',
+     *  'models/oxpsmymoduleoxarticle.php',
+     *  'controllers/oxpsmymodulepage.php',
+     *  'models/oxpsmymoduleitem.php',
+     * -----------------------------------------------------------------------------------------------
      */
     public function testRenderModuleFiles()
     {
-        // TODO DDR:
-        $this->markTestIncomplete('FIX at(X) order and add new files, fix Case and paths');
-
         // File system helper mock
-        $oFileSystem = $this->getMock('oxpsModuleGeneratorFileSystem', array('__call', 'createFile', 'renameFile'));
+        $oFileSystem = $this->getMock(
+            'oxpsModuleGeneratorFileSystem',
+            array('__call', 'createFile', 'renameFile', 'isFile')
+        );
+
+        $oFileSystem->expects($this->any())->method('isFile')->willReturn(true);
 
         /* German translation file */
-        $oFileSystem->expects($this->at(0))->method('createFile')
+        $oFileSystem->expects($this->at(1))->method('createFile')
             ->with(
-                '/path/to/modules/oxps/mymodule/translations/de/oxpsmodule_lang.php.tpl',
+                '/path/to/modules/oxps/mymodule/Application/translations/de/oxpsModule_lang.php.tpl',
                 '_processed_de_trans_content_'
             );
-        $oFileSystem->expects($this->at(1))->method('renameFile')
+        $oFileSystem->expects($this->at(2))->method('renameFile')
             ->with(
-                '/path/to/modules/oxps/mymodule/translations/de/oxpsmodule_lang.php.tpl',
-                '/path/to/modules/oxps/mymodule/translations/de/oxpsmymodule_de_lang.php'
+                '/path/to/modules/oxps/mymodule/Application/translations/de/oxpsModule_lang.php.tpl',
+                '/path/to/modules/oxps/mymodule/Application/translations/de/oxpsmymodule_de_lang.php'
             );
 
         /* Readme file */
-        $oFileSystem->expects($this->at(7))->method('createFile')
+        $oFileSystem->expects($this->at(12))->method('createFile')
             ->with('/path/to/modules/oxps/mymodule/docs/README.txt', '_processed_readme_content_');
 
         /* Extended oxArticle model */
-        $oFileSystem->expects($this->at(15))->method('createFile')
+        $oFileSystem->expects($this->at(31))->method('createFile')
             ->with('/path/to/modules/oxps/mymodule/models/oxpsmymoduleoxarticle.php', '_processed_oxarticle_content_');
 
         /* New "Item" model */
-        $oFileSystem->expects($this->at(17))->method('createFile')
+        $oFileSystem->expects($this->at(35))->method('createFile')
             ->with('/path/to/modules/oxps/mymodule/models/oxpsmymoduleitem.php', '_processed_item_content_');
 
         oxRegistry::set('oxpsModuleGeneratorFileSystem', $oFileSystem);
@@ -133,8 +137,12 @@ class oxpsModuleGeneratorRenderTest extends OxidTestCase
             'oxpsModuleGeneratorOxModule',
             array('__construct', '__call', 'getModuleId', 'getFullPath')
         );
-        $oModule->expects($this->once())->method('getModuleId')->will($this->returnValue('oxpsmymodule'));
-        $oModule->expects($this->once())->method('getFullPath')->will($this->returnValue('/path/to/modules/oxps/mymodule/'));
+        $oModule->expects($this->once())
+            ->method('getModuleId')
+            ->will($this->returnValue('oxpsmymodule'));
+        $oModule->expects($this->once())
+            ->method('getFullPath')
+            ->will($this->returnValue('/path/to/modules/oxps/mymodule/'));
 
         // Smarty mock
         /* German translation file */
@@ -145,7 +153,7 @@ class oxpsModuleGeneratorRenderTest extends OxidTestCase
             ->with('sFilePath', 'Application/translations/de/oxpsModule_lang.php.tpl');
         $oSmarty->expects($this->at(2))->method('assign')->with('sClassRealName', '');
         $oSmarty->expects($this->at(3))->method('fetch')
-            ->with('/path/to/modules/oxps/mymodule/translations/de/oxpsmodule_lang.php.tpl')
+            ->with('/path/to/modules/oxps/mymodule/Application/translations/de/oxpsModule_lang.php.tpl')
             ->will($this->returnValue('_processed_de_trans_content_'));
         $oSmarty->expects($this->at(4))->method('clear_assign')->with('sFilePath');
         $oSmarty->expects($this->at(5))->method('clear_assign')->with('sClassRealName');
@@ -157,16 +165,16 @@ class oxpsModuleGeneratorRenderTest extends OxidTestCase
             ->will($this->returnValue('_processed_readme_content_'));
 
         /* Extended oxArticle model */
-        $oSmarty->expects($this->at(46))->method('assign')->with('sFilePath', 'models/oxpsmymoduleoxarticle.php');
-        $oSmarty->expects($this->at(47))->method('assign')->with('sClassRealName', 'oxArticle');
-        $oSmarty->expects($this->at(48))->method('fetch')
+        $oSmarty->expects($this->at(56))->method('assign')->with('sFilePath', 'models/oxpsmymoduleoxarticle.php');
+        $oSmarty->expects($this->at(57))->method('assign')->with('sClassRealName', 'oxArticle');
+        $oSmarty->expects($this->at(58))->method('fetch')
             ->with('/path/to/modules/oxps/mymodule/models/oxpsmymoduleoxarticle.php')
             ->will($this->returnValue('_processed_oxarticle_content_'));
 
         /* Extended oxArticle model */
-        $oSmarty->expects($this->at(56))->method('assign')->with('sFilePath', 'models/oxpsmymoduleitem.php');
-        $oSmarty->expects($this->at(57))->method('assign')->with('sClassRealName', 'Item');
-        $oSmarty->expects($this->at(58))->method('fetch')
+        $oSmarty->expects($this->at(66))->method('assign')->with('sFilePath', 'models/oxpsmymoduleitem.php');
+        $oSmarty->expects($this->at(67))->method('assign')->with('sClassRealName', 'Item');
+        $oSmarty->expects($this->at(68))->method('fetch')
             ->with('/path/to/modules/oxps/mymodule/models/oxpsmymoduleitem.php')
             ->will($this->returnValue('_processed_item_content_'));
 
